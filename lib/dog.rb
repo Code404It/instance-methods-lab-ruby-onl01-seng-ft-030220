08c18184076704ae2f150b858eddf7a70d8e114f
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-11 12:12:24 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+def initialize 
+  bark
+end
+def bark 
+  puts "Woof!"
+end
+end
+
+class Dog 
+  def initialize
+    sit 
+  end 
+  def sit 
+    puts "The Dog is sitting"
+  end 
+end 
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+def initialize
+  talk
+end
+def talk 
+  puts "Hello World!"
+end
+end 
+
+class Person
+  def walk
+end 
+def walk 
+  puts"The Person is walking"
+end 
+end 
